6037c847e6dfd43886044f69b079991a4211b593
faizbasahil23-sketch <faizbasahil23@gmail.com>
2026-07-16 16:39:41 +0700

    M010.4 Query Handler Generator

--- backend/src/Kyvara.Builder/Generators/Application/QueryHandlerGenerator.cs
+++ backend/src/Kyvara.Builder/Generators/Application/QueryHandlerGenerator.cs
@@ -7,50 +7,20 @@ public sealed class QueryHandlerGenerator
         return """
 using MediatR;
 
-namespace {{Namespace}}.Handlers;
+namespace {{Namespace}}.Queries;
 
 public sealed class Get{{Entity}}ByIdQueryHandler
-    : IRequestHandler<Get{{Entity}}ByIdQuery, {{Entity}}Dto?>
+    : IRequestHandler<Get{{Entity}}ByIdQuery, {{Entity}}Dto>
 {
-    public async Task<{{Entity}}Dto?> Handle(
+    public async Task<{{Entity}}Dto> Handle(
         Get{{Entity}}ByIdQuery request,
         CancellationToken cancellationToken)
     {
-        // TODO: Repository call
+        // TODO:
+        // Load entity from repository
+        // Map to DTO
 
-        await Task.CompletedTask;
-
-        return default;
-    }
-}
-
-public sealed class GetAll{{Entity}}QueryHandler
-    : IRequestHandler<GetAll{{Entity}}Query,IReadOnlyList<{{Entity}}Dto>>
-{
-    public async Task<IReadOnlyList<{{Entity}}Dto>> Handle(
-        GetAll{{Entity}}Query request,
-        CancellationToken cancellationToken)
-    {
-        // TODO: Repository call
-
-        await Task.CompletedTask;
-
-        return [];
-    }
-}
-
-public sealed class Search{{Entity}}QueryHandler
-    : IRequestHandler<Search{{Entity}}Query,IReadOnlyList<{{Entity}}Dto>>
-{
-    public async Task<IReadOnlyList<{{Entity}}Dto>> Handle(
-        Search{{Entity}}Query request,
-        CancellationToken cancellationToken)
-    {
-        // TODO: Search implementation
-
-        await Task.CompletedTask;
-
-        return [];
+        return await Task.FromResult(new {{Entity}}Dto());
     }
 }
 """;
