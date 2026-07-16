13ef61ac977fa216e378539f380385246b6d9cf8
faizbasahil23-sketch <faizbasahil23@gmail.com>
2026-07-16 16:42:13 +0700

    M010.6 FluentValidation Generator

--- backend/src/Kyvara.Builder/Generators/Application/ValidatorGenerator.cs
+++ backend/src/Kyvara.Builder/Generators/Application/ValidatorGenerator.cs
@@ -9,20 +9,10 @@ using FluentValidation;
 
 namespace {{Namespace}}.Validators;
 
-public sealed class Create{{Entity}}Validator
+public sealed class Create{{Entity}}CommandValidator
     : AbstractValidator<Create{{Entity}}Command>
 {
-    public Create{{Entity}}Validator()
-    {
-        RuleFor(x => x.Id)
-            .NotEmpty();
-    }
-}
-
-public sealed class Update{{Entity}}Validator
-    : AbstractValidator<Update{{Entity}}Command>
-{
-    public Update{{Entity}}Validator()
+    public Create{{Entity}}CommandValidator()
     {
         RuleFor(x => x.Id)
             .NotEmpty();
