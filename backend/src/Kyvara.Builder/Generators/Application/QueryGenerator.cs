a3c0fd767edbbac674cb58ff8ac48b853993d10d
faizbasahil23-sketch <faizbasahil23@gmail.com>
2026-07-16 16:37:20 +0700

    M010.2 Query Generator

--- backend/src/Kyvara.Builder/Generators/Application/QueryGenerator.cs
+++ backend/src/Kyvara.Builder/Generators/Application/QueryGenerator.cs
@@ -5,17 +5,13 @@ public sealed class QueryGenerator
     public string Generate()
     {
         return """
+using MediatR;
+
 namespace {{Namespace}}.Queries;
 
 public sealed record Get{{Entity}}ByIdQuery(
     Guid Id
-);
-
-public sealed record GetAll{{Entity}}Query();
-
-public sealed record Search{{Entity}}Query(
-    string? Keyword
-);
+) : IRequest<{{Entity}}Dto>;
 """;
     }
 }
--- backend/src/Kyvara.Builder/Templates/Application/Query.tpl
+++ backend/src/Kyvara.Builder/Templates/Application/Query.tpl
@@ -1,11 +1,7 @@
+using MediatR;
+
 namespace {{Namespace}}.Queries;
 
 public sealed record Get{{Entity}}ByIdQuery(
     Guid Id
-);
-
-public sealed record GetAll{{Entity}}Query();
-
-public sealed record Search{{Entity}}Query(
-    string? Keyword
-);
+) : IRequest<{{Entity}}Dto>;
